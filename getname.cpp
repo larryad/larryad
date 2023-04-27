cdbdf9126d885711511352c0b17c8382f725aa59
larryad <132023747+larryad@users.noreply.github.com>
2023-04-28 06:40:51 +0700

    Create getname.cpp

---
+++ getname.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+  string name;
+  cout<<"Nhap vao ten";
+  cin.ignore();
+  getline(cin,name);
+  cout<<"\nDay la thong tin cua ban:\n";
+  cout<<string;
+  return 0;
+}
